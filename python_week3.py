0f1fd31f3e255dcff83e7b22a1a71380f26c365a
Kai Lee <b790718@gmail.com>
2021-03-09 13:39:33 +0800

    week3 second commit

--- python_week3.py
+++ python_week3.py
@@ -1,7 +1,21 @@
 import requests
 
+import requests
 
 url="https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json"
 response=requests.get(url)
 dataList=response.json()["result"]["results"]
-print(dataList)
+
+with open("data.txt","w",encoding="utf-8") as file :
+    for data in dataList:
+        indexJpg=data["file"].lower().find(".jpg")
+        print(data["stitle"],indexJpg)
+        if indexJpg==-1:
+            indexPng=data["file"].lower().find(".png")
+            firstPng=data["file"][:indexPng]
+            file.write(data["stitle"]+","+data["longitude"]+","+data["latitude"]+","+firstPng+".png"+"\n")
+        else:
+            firstJpg=data["file"][:indexJpg]
+            file.write(data["stitle"]+","+data["longitude"]+","+data["latitude"]+","+firstJpg+".jpg"+"\n")
+
+
