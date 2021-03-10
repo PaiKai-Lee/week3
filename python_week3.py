b1a3daadaa396ce11cf916ad1fb53e462f76700b
Kai Lee <b790718@gmail.com>
2021-03-10 14:18:00 +0800

    js first commit

--- python_week3.py
+++ python_week3.py
@@ -4,13 +4,13 @@ import requests
 
 url="https://padax.github.io/taipei-day-trip-resources/taipei-attractions.json"
 response=requests.get(url)
-dataList=response.json()["result"]["results"]
+dataList=response.json()["result"]["results"]        #抓取results內的資料
 
 with open("data.txt","w",encoding="utf-8") as file :
     for data in dataList:
-        dataFile=data["file"]
-        fileList=dataFile.split("http://")
-        firstImg=fileList[1]
+        dataFile=data["file"]                        #抓取key="file"內的資料(圖片連結網址)
+        fileList=dataFile.split("http://")           #將整串字串以列表方式分開
+        firstImg=fileList[1]                         #取的第一張圖片的連結
         print(firstImg)
         file.write(data["stitle"]+","+data["longitude"]+","+data["latitude"]+","+"http://"+firstImg+"\n")
 
